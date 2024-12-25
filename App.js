c9c181032c490f4816b88ce2fdfd66cbd669e8f7
Kazi-Reazul-Islam <kazireazulislam1999@gmail.com>
2024-12-26 00:09:08 +0600

    Nativewind Setup

---
+++ App.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const App = () => {
+  return (
+    <View className="bg-white flex-1 justify-center items-center">
+      <Text className="text-xl">Receipt Scan</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
