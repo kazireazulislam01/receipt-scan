01a2fe5c481f01cf85c9da3ae058daf401a56c92
Kazi-Reazul-Islam <kazireazulislam1999@gmail.com>
2024-12-28 17:29:46 +0600

    Simple changed add on bottom tab

--- src/navigation/MainNavigation/MainNavigation.js
+++ src/navigation/MainNavigation/MainNavigation.js
@@ -5,8 +5,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {HomeScreen, ProfileScreen, ScanScreen} from '../../screens';
 import {HomeIcon, ProfileIcon, ScanIcon} from '../../assets/icon';
-import {View} from 'react-native';
-import {Text} from 'react-native-svg';
+import {Text, View} from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
@@ -15,16 +14,33 @@ const MainNavigation = () => {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          borderRadius: 25,
+          marginHorizontal: 10,
+          marginBottom: 20,
+          position: 'absolute',
+          height: 55,
+        },
       }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
         options={{
+          tabBarLabel: ({focused}) => (
+            <View>
+              <Text
+                className={`${
+                  focused ? 'text-green-500' : 'text-gray-500'
+                } text-xs font-semibold`}>
+                Home
+              </Text>
+            </View>
+          ),
           tabBarIcon: ({focused}) => (
             <View>
               <HomeIcon
-                height={20}
                 width={20}
+                height={20}
                 color={focused ? colors.green[500] : colors.gray[500]}
               />
             </View>
@@ -35,6 +51,16 @@ const MainNavigation = () => {
         name="Scan"
         component={ScanScreen}
         options={{
+          tabBarLabel: ({focused}) => (
+            <View>
+              <Text
+                className={`${
+                  focused ? 'text-green-500' : 'text-gray-500'
+                } text-xs font-semibold`}>
+                Scan
+              </Text>
+            </View>
+          ),
           tabBarIcon: ({focused}) => (
             <View>
               <ScanIcon
@@ -50,6 +76,16 @@ const MainNavigation = () => {
         name="Profile"
         component={ProfileScreen}
         options={{
+          tabBarLabel: ({focused}) => (
+            <View>
+              <Text
+                className={`${
+                  focused ? 'text-green-500' : 'text-gray-500'
+                } text-xs font-semibold`}>
+                Profile
+              </Text>
+            </View>
+          ),
           tabBarIcon: ({focused}) => (
             <View>
               <ProfileIcon
